c126ef078d4f21824fec49d38728013f6b2c2c82
Sebastian Bürgel <s.buergel@gmail.com>
2021-01-09 22:41:41 +0000

    add first block data

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,28 @@ async function search() {
     let blockNo = parseInt(query, 10);
     console.log("searching block number " + blockNo);
     let block = await proxiedWeb3.eth.getBlock(blockNo);
-    document.getElementById("results").innerText = "block information: " + JSON.stringify(block);
+    let results = document.getElementById("results");
+    var table = document.createElement("table");
+    results.appendChild(table);
+    var th = document.createElement("th");
+    table.appendChild(th);
+    var td11 = document.createElement("td");
+    th.appendChild(td11);
+    td11.innerText = "Block number";
+    var td12 = document.createElement("td");
+    th.appendChild(td12);
+    td12.innerText = "Timestamp";
+    
+    var tr = document.createElement("tr");
+    table.appendChild(tr);
+    var td21 = document.createElement("td");
+    tr.appendChild(td21);
+    td21.innerText = block.number;
+    var td22 = document.createElement("td");
+    tr.appendChild(td22);
+    td22.innerText = new Date(t*1000).toLocaleDateString() + " - " + ew Date(t*1000).toLocaleTimeString();
+
+    // {"difficulty":"17179869184","extraData":"0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa","gasLimit":5000,"gasUsed":0,"hash":"0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3","logsBloom":"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000","miner":"0x0000000000000000000000000000000000000000","mixHash":"0x0000000000000000000000000000000000000000000000000000000000000000","nonce":"0x0000000000000042","number":0,"parentHash":"0x0000000000000000000000000000000000000000000000000000000000000000","receiptsRoot":"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421","sha3Uncles":"0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347","size":540,"stateRoot":"0xd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544","timestamp":0,"totalDifficulty":"17179869184","transactions":[],"transactionsRoot":"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421","uncles":[]}
   }
   else if (query.length == 42) { // assume this is an address
     console.log("searching tx or block hash " + query);
