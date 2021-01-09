a0ed682c9b5958aba6d15a52f5201308e7b37f1a
Sebastian Bürgel <s.buergel@gmail.com>
2021-01-09 23:37:42 +0000

    removed previous result

--- js/scripts.js
+++ js/scripts.js
@@ -57,11 +57,12 @@ function createBlockTable(tableData) {
 async function search() {
   let query = document.getElementById("query").value;
   query = query.replace(/ /g,'');
+  let results = document.getElementById("results");
+  results.innerHTML = "";
+
   if (query.length < 42) { // assume this is a block number
     let blockNo = parseInt(query, 10);
-    console.log("searching block number " + blockNo);
     let block = await proxiedWeb3.eth.getBlock(blockNo);
-    let results = document.getElementById("results");
 
     let tableData = [
       [ "Block number:", block.number ],
@@ -88,8 +89,6 @@ async function search() {
 
     let table = createBlockTable(tableData);
     results.appendChild(table);
-
-    // {"difficulty":"17179869184","extraData":"0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa","gasLimit":5000,"gasUsed":0,"hash":"0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3","logsBloom":"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000","miner":"0x0000000000000000000000000000000000000000","mixHash":"0x0000000000000000000000000000000000000000000000000000000000000000","nonce":"0x0000000000000042","number":0,"parentHash":"0x0000000000000000000000000000000000000000000000000000000000000000","receiptsRoot":"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421","sha3Uncles":"0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347","size":540,"stateRoot":"0xd7f8974fb5ac78d9ac099b9ad5018bedc2ce0a72dad1827a1709da30580f0544","timestamp":0,"totalDifficulty":"17179869184","transactions":[],"transactionsRoot":"0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421","uncles":[]}
   }
   else if (query.length == 42) { // assume this is an address
     console.log("searching tx or block hash " + query);
